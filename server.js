f2069f1e849a1948817b83515ec787cf62e00d2d
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-13 13:34:32 -0300

    used logger in server.js, created public folder and index.html to serve static

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 //? 1: Import the express module
 const express = require('express');
-const router = require('./routes/users');
-
 //? 2. Creates an express application
-const app = express()
-
-//adding ejs as view engine
+const app = express();
+//? adding ejs as view engine
 app.set('view engine', 'ejs')
+//?//? middleware function that logs out the request url
+app.use(logger)
+//? 5. Import the userRouter
+const userRouter = require('./routes/users');
 
 //! 4. BREAD AND BUTTER - Create routes for the app; RESTful APIs. Standard HTTP request/respond methods
 //? Request data from the server
@@ -16,10 +17,6 @@ app.get('/', (req, res) => {
     //res.status(200).send('This is the response being sent to the user.')
     res.render('index', { text: "This is text in the server.js file that is going to show up on the browser but I'm only going to type <%= text %> in my index.ejs file to display it  - cool!" })
 });
-
-
-
-
 
 //? Post/Send/Submit data to the server. 
 // app.post()
@@ -33,11 +30,14 @@ app.get('/', (req, res) => {
 //? Used to delete resources
 // app.delete()
 
-const userRouter = require('./routes/users')
-
-
 //! tells server, anything that starts with /users, add all the routes in userRouter to it
 app.use('/users', userRouter)
+
+//? Logging out something function
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 //? 3. Tell the app to listen on port 3001
 app.listen(3001);
