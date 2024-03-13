5a79c348537f55fe24ac252e11e6cdb1a36b9087
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-13 00:34:52 -0300

    config user routes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //? 1: Import the express module
-const express = require('express')
+const express = require('express');
+const router = require('./routes/users');
 
 //? 2. Creates an express application
 const app = express()
+
+//adding ejs as view engine
 app.set('view engine', 'ejs')
 
 //! 4. BREAD AND BUTTER - Create routes for the app; RESTful APIs. Standard HTTP request/respond methods
@@ -10,21 +13,31 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     console.log('Here')
     // res.sendStatus(200) can not be chained but .status can
-    res.status(200).send('This is the response being sent to the user.')
-    res.render('index')
+    //res.status(200).send('This is the response being sent to the user.')
+    res.render('index', { text: "This is text in the server.js file that is going to show up on the browser but I'm only going to type <%= text %> in my index.ejs file to display it  - cool!" })
 });
+
+
+
+
 
 //? Post/Send/Submit data to the server. 
 // app.post()
 
-// //? Used for updating or replacing an entire resource on the server.
+//? Used for updating or replacing an entire resource on the server.
 // app.put()
 
-// //? Used to apply partial modifications to a resource. 
+//? Used to apply partial modifications to a resource. 
 // app.patch()
 
-
+//? Used to delete resources
 // app.delete()
+
+const userRouter = require('./routes/users')
+
+
+//! tells server, anything that starts with /users, add all the routes in userRouter to it
+app.use('/users', userRouter)
 
 //? 3. Tell the app to listen on port 3001
 app.listen(3001);
