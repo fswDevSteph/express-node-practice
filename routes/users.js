478bf8909e3bed14ba1fe07b76f13a9305df8c08
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-13 01:33:18 -0300

    add string literal syntax

--- routes/users.js
+++ routes/users.js
@@ -18,30 +18,38 @@ router.post('/', (req, res) => {
 //?_____________________________________________________________
 //! chain together all get put delete routes
 router.route('/:id').get((req, res) => {
-    res.send('User Get with ID ${req.params.id}')
+    console.log(req.user)
+    res.send(`Get user with ID ${req.params.id}`)
 }).put((req, res) => {
-    res.send('Update User with ID ${req.params.id}')
+    res.send(`Update User with ID ${req.params.id}`)
 }).delete((req, res) => {
     req.params.id
-    res.send('Delete Get with ID ${req.params.id}')
+    res.send(`Delete user with ID ${req.params.id}`)
 })
 //? --------------------------------------------------------------------
 //! original RESTful API routes below:
-router
-    .route('/:id')
-    .get('/:id', (req, res) => {
-        req.params.id
-        res.send('User Get with ID ${req.params.id}')
-    })
+// router
+//     .route('/:id')
+//     .get('/:id', (req, res) => {
+//         req.params.id
+//         res.send('User Get with ID ${req.params.id}')
+//     })
 
-router.put('/:id', (req, res) => {
-    req.params.id
-    res.send('Update User with ID ${req.params.id}')
-})
+// router.put('/:id', (req, res) => {
+//     req.params.id
+//     res.send('Update User with ID ${req.params.id}')
+// })
 
-router.delete('/:id', (req, res) => {
-    req.params.id
-    res.send('Delete Get with ID ${req.params.id}')
+// router.delete('/:id', (req, res) => {
+//     req.params.id
+//     res.send('Delete Get with ID ${req.params.id}')
+// })
+//?router.param says, hey whenevr you see a param that matches what i passed in, in this case, 'ID' I want you to run this code with the request, response next and id.
+
+const users = [{ name: 'Steph' }, { name: 'Sam' }, { name: 'SamX2?' }, { name: 'Saurahb' }, { name: 'Daniel' }]
+router.param('id', (req, res, next, id) => {
+    req.user = users[id]
+    next()
 })
 
 module.exports = router
